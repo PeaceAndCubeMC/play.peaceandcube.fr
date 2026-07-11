607b569887c476760b3f5d36f8a4812f9564c571
YanisBft <yanis.briffaut@gmail.com>
2026-07-11 20:11:01 +0200

    Reformat files

--- MinecraftVotifier.php
+++ MinecraftVotifier.php
@@ -16,65 +16,70 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-class MinecraftVotifier {
-	const VOTE_FORMAT       = "VOTE\n%s\n%s\n%s\n%d\n";
-	const PUBLIC_KEY_FORMAT = "-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----";
+class MinecraftVotifier
+{
+    const VOTE_FORMAT = "VOTE\n%s\n%s\n%s\n%d\n";
+    const PUBLIC_KEY_FORMAT = "-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----";
 
-	private $public_key;
-	private $server_ip;
-	private $port;
-	private $service_name;
+    private $public_key;
+    private $server_ip;
+    private $port;
+    private $service_name;
 
-	public function __construct($public_key = null, $server_ip = null, $port = 8192, $service_name = null) {
-		$this->public_key   = $this->formatPublicKey($public_key);
-		$this->server_ip    = $server_ip;
-		$this->port         = $port;
-		$this->service_name = $service_name;
-	}
+    public function __construct($public_key = null, $server_ip = null, $port = 8192, $service_name = null)
+    {
+        $this->public_key = $this->formatPublicKey($public_key);
+        $this->server_ip = $server_ip;
+        $this->port = $port;
+        $this->service_name = $service_name;
+    }
 
-	public function __get($name) {
-		return isset($this->$name) ? $this->$name : null;
-	}
+    public function __get($name)
+    {
+        return isset($this->$name) ? $this->$name : null;
+    }
 
-	public function __set($name, $value) {
-		if ($name == 'public_key')
-			$this->public_key = $this->formatPublicKey($value);
-		else
-			$this->$name = $value;
-	}
+    public function __set($name, $value)
+    {
+        if ($name == 'public_key')
+            $this->public_key = $this->formatPublicKey($value);
+        else
+            $this->$name = $value;
+    }
 
-	private function formatPublicKey($public_key) {
-		$public_key = wordwrap($public_key, 65, "\n", true);
-		$public_key = sprintf(self::PUBLIC_KEY_FORMAT, $public_key);
+    private function formatPublicKey($public_key)
+    {
+        $public_key = wordwrap($public_key, 65, "\n", true);
+        $public_key = sprintf(self::PUBLIC_KEY_FORMAT, $public_key);
 
-		return $public_key;
-	}
+        return $public_key;
+    }
 
-	public function sendVote($username) {
-		if (php_sapi_name() !== 'cli') {
-			// Use Client Address BEHIND Proxy if it is a transparent proxy...
-			$address = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER['REMOTE_ADDR'];
-		}
-		
-		else {
-			// Script is run by CLI, use our own Name
-			$address = $_SERVER["HOST_NAME"];
-		}
-		
-		$vote = sprintf(self::VOTE_FORMAT, $this->service_name, $username, $address, time());
+    public function sendVote($username)
+    {
+        if (php_sapi_name() !== 'cli') {
+            // Use Client Address BEHIND Proxy if it is a transparent proxy...
+            $address = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER['REMOTE_ADDR'];
+        } else {
+            // Script is run by CLI, use our own Name
+            $address = $_SERVER["HOST_NAME"];
+        }
 
-		openssl_public_encrypt($vote, $data, $this->public_key);
+        $vote = sprintf(self::VOTE_FORMAT, $this->service_name, $username, $address, time());
 
-		$socket = @fsockopen($this->server_ip, $this->port);
+        openssl_public_encrypt($vote, $data, $this->public_key);
 
-		if ($socket) {
-			if (fwrite($socket, $data)) {
-				fclose($socket);
-				return true;
-			}
-		}
+        $socket = @fsockopen($this->server_ip, $this->port);
 
-		return false;
-	}
+        if ($socket) {
+            if (fwrite($socket, $data)) {
+                fclose($socket);
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
+
 ?>
--- dovote.php
+++ dovote.php
@@ -12,86 +12,68 @@ $response = [];
 
 $shouldContinue = false;
 if (check_site_1($response) && check_site_2($response) && check_site_3($response)) {
-  $shouldContinue = true;
+    $shouldContinue = true;
 }
 
 if ($shouldContinue) {
-  $username = $_POST["username"];
+    $username = $_POST["username"];
 
-  if (empty($username)) {
-    $response["state"] = "USERNAME_EMPTY";
-    $response["message"] = "Tu n'as pas renseigné ton pseudo !";
-    echo json_encode($response);
-    exit();
-  }
+    if (empty($username)) {
+        $response["state"] = "USERNAME_EMPTY";
+        $response["message"] = "Tu n'as pas renseigné ton pseudo !";
+        echo json_encode($response);
+        exit();
+    }
 
-  // Display everything in browser, because some people can't look in logs for errors
-  Error_Reporting(E_ALL | E_STRICT);
-  Ini_Set('display_errors', true);
+    // Display everything in browser, because some people can't look in logs for errors
+    Error_Reporting(E_ALL | E_STRICT);
+    Ini_Set('display_errors', true);
 
-  $query = new MinecraftQuery();
-  $timer = MicroTime(true);
+    $query = new MinecraftQuery();
+    $timer = MicroTime(true);
 
-  try {
-      $query->Connect($_ENV["MC_SERVER_IP"], $_ENV["MC_SERVER_PORT"], 1);
-  }
-  catch (MinecraftQueryException $e) {
-      $Exception = $e;
-      echo $e;
-  }
+    try {
+        $query->Connect($_ENV["MC_SERVER_IP"], $_ENV["MC_SERVER_PORT"], 1);
+    } catch (MinecraftQueryException $e) {
+        $Exception = $e;
+        echo $e;
+    }
 
-  $timer = Number_Format(MicroTime(true) - $timer, 4, '.', '');
+    $timer = Number_Format(MicroTime(true) - $timer, 4, '.', '');
 
-  if (($Players = $query->GetPlayers()) !== false) {
+    if (($players = $query->GetPlayers()) !== false) {
 
-    if (in_array($username, $Players)) {
+        if (in_array($username, $players)) {
 
-        try {
-            $pdo = new PDO('sqlite:' . dirname(__FILE__) . '/votes.sqlite');
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
-            $pdo->query("CREATE TABLE IF NOT EXISTS votes (
+            try {
+                $pdo = new PDO('sqlite:' . dirname(__FILE__) . '/votes.sqlite');
+                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
+                $pdo->query("CREATE TABLE IF NOT EXISTS votes (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               pseudo VARCHAR(20),
               created DATETIME,
               last DATETIME,
               totalvote INTEGER
             )");
-        }
-        catch (Exception $e) {
-            echo "Impossible d'accéder à la base de données SQLite : " . $e->getMessage();
-            die();
-        }
+            } catch (Exception $e) {
+                echo "Impossible d'accéder à la base de données SQLite : " . $e->getMessage();
+                die();
+            }
 
-        $exist = $pdo->prepare("SELECT * from votes where pseudo = :val");
-        $exist->execute(array(
-            ':val' => $username
-        ));
-        $reception = empty($exist);
-        $com       = 0;
-        $row       = $exist->fetch(PDO::FETCH_ASSOC);
+            $exist = $pdo->prepare("SELECT * from votes where pseudo = :val");
+            $exist->execute(array(
+                ':val' => $username
+            ));
+            $reception = empty($exist);
+            $com = 0;
+            $row = $exist->fetch(PDO::FETCH_ASSOC);
 
-        if (empty($row)) {
-            // Nouveau joueur
-            $req = $pdo->prepare("INSERT INTO votes (pseudo,created,last,totalvote) VALUES(:pseudo,DATETIME('NOW'),DATETIME('NOW'),1)");
-            $req->bindValue(':pseudo', $username);
-            $result = $req->execute();
-
-            require 'MinecraftVotifier.php';
-            $votifier = new MinecraftVotifier($_ENV["VOTIFIER_PUBLIC_KEY"], $_ENV["MC_SERVER_IP"], '8192', 'vote');
-            $votifier->sendVote($username);
-
-            $response["state"] = "SUCCESS";
-            $response["message"] = "Merci d'avoir voté !";
-        }
-        else {
-            $d1 = new DateTime(date("Y-m-d H:i:s"));
-            $d2 = new DateTime(($row['last']));
-            $di = date_diff($d2, $d1)->format('%d');
-            if ($di != 0) {
-                $req2 = $pdo->prepare("UPDATE votes set totalvote = totalvote+1, last = DATETIME('NOW') where pseudo=:pseudo");
-                $req2->bindValue(':pseudo', $username);
-                $result2 = $req2->execute();
+            if (empty($row)) {
+                // Nouveau joueur
+                $req = $pdo->prepare("INSERT INTO votes (pseudo,created,last,totalvote) VALUES(:pseudo,DATETIME('NOW'),DATETIME('NOW'),1)");
+                $req->bindValue(':pseudo', $username);
+                $result = $req->execute();
 
                 require 'MinecraftVotifier.php';
                 $votifier = new MinecraftVotifier($_ENV["VOTIFIER_PUBLIC_KEY"], $_ENV["MC_SERVER_IP"], '8192', 'vote');
@@ -100,72 +82,89 @@ if ($shouldContinue) {
                 $response["state"] = "SUCCESS";
                 $response["message"] = "Merci d'avoir voté !";
             } else {
-                $date_expire = $d2->modify('+1 day');
-                $response["state"] = "ALREADY_VOTED";
-                $response["message"] = "Tu as déjà voté ces dernières 24h ! Encore " . date_diff($date_expire, $d1)->format('%H heures %i minutes %s secondes avant de voter.');
+                $d1 = new DateTime(date("Y-m-d H:i:s"));
+                $d2 = new DateTime(($row['last']));
+                $di = date_diff($d2, $d1)->format('%d');
+                if ($di != 0) {
+                    $req2 = $pdo->prepare("UPDATE votes set totalvote = totalvote+1, last = DATETIME('NOW') where pseudo=:pseudo");
+                    $req2->bindValue(':pseudo', $username);
+                    $result2 = $req2->execute();
+
+                    require 'MinecraftVotifier.php';
+                    $votifier = new MinecraftVotifier($_ENV["VOTIFIER_PUBLIC_KEY"], $_ENV["MC_SERVER_IP"], '8192', 'vote');
+                    $votifier->sendVote($username);
+
+                    $response["state"] = "SUCCESS";
+                    $response["message"] = "Merci d'avoir voté !";
+                } else {
+                    $date_expire = $d2->modify('+1 day');
+                    $response["state"] = "ALREADY_VOTED";
+                    $response["message"] = "Tu as déjà voté ces dernières 24h ! Encore " . date_diff($date_expire, $d1)->format('%H heures %i minutes %s secondes avant de voter.');
+                }
             }
+        } else {
+            $response["state"] = "NOT_CONNECTED";
+            $response["message"] = "Tu n'es pas connecté sur le serveur !";
         }
     } else {
-      $response["state"] = "NOT_CONNECTED";
-      $response["message"] = "Tu n'es pas connecté sur le serveur !";
+        echo "Erreur";
     }
-  } else {
-    echo "Erreur";
-  }
 } else {
-  $response["state"] = "NOT_VOTED";
+    $response["state"] = "NOT_VOTED";
 }
 
 echo json_encode($response);
 
-function check_site_1(&$response) {
-  $serverToken = $_ENV["SITE_1_TOKEN"]; // Token de votre serveur
-  $remoteAddr = $_SERVER['REMOTE_ADDR']; // Adresse IP de l'utilisateur
-  $json = file_get_contents("https://serveur-prive.net/api/vote/json/$serverToken/$remoteAddr");
-  $json_data = json_decode($json);
+function check_site_1(&$response)
+{
+    $serverToken = $_ENV["SITE_1_TOKEN"]; // Token de votre serveur
+    $remoteAddr = $_SERVER['REMOTE_ADDR']; // Adresse IP de l'utilisateur
+    $json = file_get_contents("https://serveur-prive.net/api/vote/json/$serverToken/$remoteAddr");
+    $json_data = json_decode($json);
 
-  if($json_data->status == 1) {
-    // Vous pouvez utiliser les variables suivantes :
-    $json_data->vote; // Correspond à la date du vote au format timestamp
-    $json_data->nextvote; // Correspond au nombre de secondes restantes avant que l'utilisateur puisse à nouveau voter
-    $json_data->pseudo; // Pseudonyme de l'utilisateur (si il a spécifié son pseudo lors de son vote)
-    
-    return true;
-  }
-  else {
-    $response["message"] = "Tu n'as pas voté sur serveur-prive.net !";
-    return false;
-  }
+    if ($json_data->status == 1) {
+        // Vous pouvez utiliser les variables suivantes :
+        $json_data->vote; // Correspond à la date du vote au format timestamp
+        $json_data->nextvote; // Correspond au nombre de secondes restantes avant que l'utilisateur puisse à nouveau voter
+        $json_data->pseudo; // Pseudonyme de l'utilisateur (si il a spécifié son pseudo lors de son vote)
+
+        return true;
+    } else {
+        $response["message"] = "Tu n'as pas voté sur serveur-prive.net !";
+        return false;
+    }
 }
 
-function check_site_2(&$response) {
-  $serverId = $_ENV["SITE_2_SERVER_ID"];
-  $remoteAddr = $_SERVER['REMOTE_ADDR'];
-  $apiUrl = "https://www.serveursminecraft.org/sm_api/peutVoter.php?id=$serverId&ip=$remoteAddr";
-  $apiResult = file_get_contents($apiUrl);
+function check_site_2(&$response)
+{
+    $serverId = $_ENV["SITE_2_SERVER_ID"];
+    $remoteAddr = $_SERVER['REMOTE_ADDR'];
+    $apiUrl = "https://www.serveursminecraft.org/sm_api/peutVoter.php?id=$serverId&ip=$remoteAddr";
+    $apiResult = file_get_contents($apiUrl);
 
-  if ($apiResult == "true") {
-    $response["message"] = "Tu n'as pas voté sur serveursminecraft.org !";
-    return false;
-  } else {
-    // return $apiResult; // la variable donne le nombre de seconde restant.
-    return true;
-  }
+    if ($apiResult == "true") {
+        $response["message"] = "Tu n'as pas voté sur serveursminecraft.org !";
+        return false;
+    } else {
+        // return $apiResult; // la variable donne le nombre de seconde restant.
+        return true;
+    }
 }
 
-function check_site_3(&$response) {
-  $serverToken = $_ENV["SITE_3_TOKEN"]; // Token de votre serveur
-  $remoteAddr = $_SERVER['REMOTE_ADDR'];
-  $apiUrl = "https://api.top-serveurs.net/v1/votes/check-ip?server_token=$serverToken&ip=$remoteAddr";
-  $apiResult = file_get_contents($apiUrl);
-  $json = json_decode($apiResult);
+function check_site_3(&$response)
+{
+    $serverToken = $_ENV["SITE_3_TOKEN"]; // Token de votre serveur
+    $remoteAddr = $_SERVER['REMOTE_ADDR'];
+    $apiUrl = "https://api.top-serveurs.net/v1/votes/check-ip?server_token=$serverToken&ip=$remoteAddr";
+    $apiResult = file_get_contents($apiUrl);
+    $json = json_decode($apiResult);
 
-  if ($json->success) {
-    return true;
-  } else {
-    $response["message"] = "Tu n'as pas voté sur top-serveurs.net !";
-    return false;
-  }
+    if ($json->success) {
+        return true;
+    } else {
+        $response["message"] = "Tu n'as pas voté sur top-serveurs.net !";
+        return false;
+    }
 }
 
 ?>
